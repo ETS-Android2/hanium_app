26ebc6cd16bce274088ac10a6bff189560bf0435
ATH <seof622@naver.com>
2021-07-15 14:16:49 +0900

    re-first commit

--- app/src/main/java/com/example/app_java/Control.java
+++ app/src/main/java/com/example/app_java/Control.java
@@ -3,12 +3,33 @@ package com.example.app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class Control extends AppCompatActivity {
 
+    private MqttAndroidClient mqttAndroidClient = null;
+    private IMqttToken token;
+    private EditText ET_User;
+    private ImageView mqtt_image;
     Button btnset;
 
     @Override
@@ -16,7 +37,72 @@ public class Control extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
         Button btnset = (Button) findViewById(R.id.btnset);
+        ET_User = findViewById(R.id.ET_User);
+        mqtt_image = findViewById(R.id.mqtt_img);
+        token = null;
+        mqttAndroidClient = new MqttAndroidClient(this,  "tcp://" + "54.185.18.26" + ":1883", MqttClient.generateClientId());
 
+        // 2번째 파라메터 : 브로커의 ip 주소 , 3번째 파라메터 : client 의 id를 지정함 여기서는 paho 의 자동으로 id를 만들어주는것
+        try {
+            token = mqttAndroidClient.connect(getMqttConnectionOption());    //mqtttoken 이라는것을 만들어 connect option을 달아줌
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    mqttAndroidClient.setBufferOpts(getDisconnectedBufferOptions());    //연결에 성공한경우
+
+                    Log.e("Connect_success", "Success");
+                    try {
+                        mqttAndroidClient.subscribe("common", 0 );   //연결에 성공하면 common 라는 토픽으로 subscribe함
+                        mqttAndroidClient.subscribe("picture",0);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {   //연결에 실패한경우
+                    Log.e("connect_fail", "Failure " + exception.toString());
+                }
+            });
+        } catch (MqttException e)
+        {
+            e.printStackTrace();
+        }
+/*
+        try {
+            mqttAndroidClient.subscribe("common", 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+*/
+        mqttAndroidClient.setCallback(new MqttCallback() {  //클라이언트의 콜백을 처리하는부분
+
+            @Override
+            public void connectionLost(Throwable cause) {
+            }
+
+            @Override
+
+            public void messageArrived(String topic, MqttMessage message) throws Exception {    //모든 메시지가 올때 Callback method
+                if (topic.equals("common")){     //topic 별로 분기처리하여 작업을 수행할수도있음
+                    String msg = new String(message.getPayload());
+                    ET_User.requestFocus();
+                    ET_User.setText(msg);
+                    Log.e("arrive message : ", msg);
+                }
+                else if(topic.equals("picture")){
+                    byte[] $byteArray = message.getPayload();
+                    Bitmap bitmap = BitmapFactory.decodeByteArray( $byteArray, 0, $byteArray.length ) ;
+                    mqtt_image.setImageBitmap(bitmap);
+                }
+            }
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+            }
+        });
 
         btnset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,4 +113,39 @@ public class Control extends AppCompatActivity {
             }
         });
     }
+
+
+    private DisconnectedBufferOptions getDisconnectedBufferOptions() {
+
+        DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+
+        disconnectedBufferOptions.setBufferEnabled(true);
+
+        disconnectedBufferOptions.setBufferSize(100);
+
+        disconnectedBufferOptions.setPersistBuffer(true);
+
+        disconnectedBufferOptions.setDeleteOldestMessages(false);
+
+        return disconnectedBufferOptions;
+
+    }
+
+
+
+    private MqttConnectOptions getMqttConnectionOption() {
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+
+        mqttConnectOptions.setCleanSession(false);
+
+        mqttConnectOptions.setAutomaticReconnect(true);
+
+        mqttConnectOptions.setWill("aaa", "I am going offline".getBytes(), 1, true);
+
+        return mqttConnectOptions;
+
+    }
+
 }
+
