039bf070e72dda86d7cd2819f8ca8fee43441caa
ATH <seof622@naver.com>
2021-07-26 04:05:02 +0900

    improve password number, add visible/invisible function

--- app/src/main/java/com/example/app_java/appLock.java
+++ app/src/main/java/com/example/app_java/appLock.java
@@ -8,13 +8,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.app_java.R;
 import com.example.app_java.app_lock_const;
@@ -25,24 +28,23 @@ public class appLock extends AppCompatActivity {
 
     private String oldPwd = "";
     private String newpwd = "";
-    private String[] pwd = new String[4];
+    private int pwd_index;
     private boolean changePwdUnlock = false;
     private Animation anim;
 
     SharedPreferences sharePref;
     SharedPreferences.Editor editor;
-    ImageView IV1;
-    ImageView IV2;
-    ImageView IV3;
-    ImageView IV4;
+    EditText et_pwd;
+    Button pwd_set_btn;
+    ImageButton Visible_btn;
     TextView Info;
-    int currentValue = -1;
+    int currentValue;
+    private String pwd;
     private Button[] btn_array = null;
     private int btnum;
-    private boolean start;
     private int randnum1 ,randnum2;
     private Random rand = new Random();
-
+    private String fake_num;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,15 +54,17 @@ public class appLock extends AppCompatActivity {
         sharePref = this.getSharedPreferences("appLock", Context.MODE_PRIVATE);
         editor = sharePref.edit();
 
-        IV1 = (ImageView)findViewById(R.id.ImageView1);
-        IV2 = (ImageView)findViewById(R.id.ImageView2);
-        IV3 = (ImageView)findViewById(R.id.ImageView3);
-        IV4 = (ImageView)findViewById(R.id.ImageView4);
+        et_pwd = (EditText)findViewById(R.id.text_pwd);
+        pwd_set_btn = (Button)findViewById(R.id.set_pwd);
+        Visible_btn = (ImageButton)findViewById(R.id.visible_btn);
         Info = (TextView)findViewById(R.id.etInputInfo);
         btn_array = new Button[12];
         randnum1 = 0;
         randnum2 = 0;
         btnum = 0;
+        pwd_index = 0;
+        fake_num = "";
+        pwd = "";
         int[] btn_id = {
                 R.id.btn0,R.id.btn1,R.id.btn2,R.id.btn3,R.id.btn4,R.id.btn5,
                 R.id.btn6,R.id.btn7,R.id.btn8,R.id.btn9,R.id.btnClear,R.id.btnErase
@@ -73,10 +77,37 @@ public class appLock extends AppCompatActivity {
         anim.setDuration(1000);
         anim.setStartOffset(20);
 
+        pwd_set_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int type = getIntent().getIntExtra("type", 0);
+                inputType(type);
+            }
+        });
+        Visible_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int status = event.getAction();
+                et_pwd.setText(fake_num);
+
+                if(status == MotionEvent.ACTION_DOWN){
+                    et_pwd.setText(pwd);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
 
     }
 
-    public void Rand_ani(){
+    public void Rand_ani(int btn){
+        String strcurrnetvalue = String.valueOf(btn);
+        pwd = pwd + strcurrnetvalue;
+        fake_num = fake_num + "*";
+        pwd_index++;
         randnum1 = rand.nextInt(12);
         randnum2 = rand.nextInt(12);
 
@@ -87,61 +118,76 @@ public class appLock extends AppCompatActivity {
 
     public void onClick(View view) {
 
+        if(pwd == ""){
+            pwd_index = 0;
+        }
 
         switch (view.getId()) {
             case R.id.btn0:
                 currentValue = 0;
-                Rand_ani();
+                Rand_ani(currentValue);
 
                 break;
             case R.id.btn1:
                 currentValue = 1;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn2:
                 currentValue = 2;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn3:
                 currentValue = 3;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn4:
                 currentValue = 4;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn5:
                 currentValue = 5;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn6:
                 currentValue = 6;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn7:
                 currentValue = 7;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn8:
                 currentValue = 8;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btn9:
                 currentValue = 9;
-                Rand_ani();
+                Rand_ani(currentValue);
                 break;
             case R.id.btnClear:
-                onClear();
-                Rand_ani();
+                pwd = "";
+                fake_num = "";
+                pwd_index = 0;
+                break;
             case R.id.btnErase:
-                onDeleteKey();
-                Rand_ani();
+                //onDeleteKey();
+                if(pwd_index == 1 ){
+                    pwd = "";
+                    fake_num = "";
+                    pwd_index = pwd_index - 1;
+                    break;
+                }else if(pwd_index > 1){
+                    fake_num = fake_num.substring(0, pwd_index - 1);
+                    pwd = pwd.substring(0, pwd_index - 1);
+                    pwd_index = pwd_index - 1;
+                    break;
+                }
         }
 
-        String strCurrentValue = String.valueOf(currentValue);
-        IV1.requestFocus();
+        et_pwd.setText(fake_num);
 
-
+    }
+/*
         if(currentValue != -1) {
             if(pwd[0] == null && pwd[1] == null && pwd[2] == null && pwd[3] == null ) {
                 //setEditText(Et1, Et2, strCurrentValue);
@@ -169,46 +215,18 @@ public class appLock extends AppCompatActivity {
                 pwd[3] = strCurrentValue;
             }
         }
+*/
 
-        if(!(pwd[0] == null) && !(pwd[1] == null) && !(pwd[2] == null) && !(pwd[3] == null) ){
-            int type = this.getIntent().getIntExtra("type",0);
-            inputType(type);
-        }
 
-    }
-    private void onDeleteKey(){
-        currentValue = -1;
-        if(IV1.isFocused()){
-            pwd[0] = null;
-        }
-        else if(IV2.isFocused()){
-            pwd[0] = null;
-            IV1.requestFocus();
-        }
-        else if(IV3.isFocused()){
-            pwd[1] = null;
-            IV2.requestFocus();
-        }
-        else if(IV4.isFocused()){
-            pwd[2] = null;
-            IV3.requestFocus();
-        }
-    }
 
     private void onClear() {
-        pwd[0] = null;
-        pwd[1] = null;
-        pwd[2] = null;
-        pwd[3] = null;
-        IV1.setImageResource(R.drawable.safe_close);
-        IV2.setImageResource(R.drawable.safe_close);
-        IV3.setImageResource(R.drawable.safe_close);
-        IV4.setImageResource(R.drawable.safe_close);
-        IV1.requestFocus();
+        pwd = "";
+        fake_num = "";
+        et_pwd.setText("");
     }
 
     public String inputPassword(){
-        return pwd[0] + pwd[1] + pwd[2] + pwd[3];
+        return pwd;
     }
 
     private  void inputType(int type) {
@@ -231,7 +249,7 @@ public class appLock extends AppCompatActivity {
                 }
             }
         }
-/*
+
         else if (type == app_lock_const.DISABLE_PASSLOCK) {
             if (isPassLockSet()) {
                 if (checkPassLock(inputPassword())) {
@@ -247,7 +265,8 @@ public class appLock extends AppCompatActivity {
                 finish();
             }
         }
-*/////////금고 비활성화
+////////금고 비활성화
+
         else if (type == app_lock_const.UNLOCK_PASSWORD) {
             if (checkPassLock(inputPassword())) {
                 setResult(Activity.RESULT_OK);
